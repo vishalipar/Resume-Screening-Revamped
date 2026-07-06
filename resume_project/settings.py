48aecb4a7547a25c73592631846fd2b5de740d89
vishalipar <vishalipar04@gmail.com>
2026-07-06 16:40:10 +0530

    change email host to brevo

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -173,11 +173,14 @@ from decouple import config
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 GROQ_API_KEY = config('GROQ_API_KEY')
 
